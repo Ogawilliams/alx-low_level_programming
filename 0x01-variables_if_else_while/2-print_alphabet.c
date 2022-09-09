6dda33894823e022d391ef816c1f2fd38b91b738
Williams Ichigboja <williamseichigboja@gmail.com>
2022-09-09 12:47:54 -0700

    4-print_alphabt

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - Prints the alphabet.
  *
@@ -8,7 +9,8 @@ int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-for (i = 0; i < 26; i++)
+
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
 	}
