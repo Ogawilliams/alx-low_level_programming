5ab1ada968073a3dd50aacebaf41346062785262
Williams Ichigboja <williamseichigboja@gmail.com>
2022-10-05 17:34:43 -0700

    How about this

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,67 +1,51 @@
-#include "main.h"
 #include <stdlib.h>
 
 /**
- * ch_free_grid - frees a 2 dimensional array.
- * @grid: multidimensional array of char.
- * @height: height of the array.
- *
- * Return: no return
- */
-void ch_free_grid(char **grid, unsigned int height)
-{
-	if (grid != NULL && height != 0)
-	{
-		for (; height > 0; height--)
-			free(grid[height]);
-		free(grid[height]);
-		free(grid);
-	}
-}
+* argstostr - Concatenates all the arguments of the program
+*
+* @ac: Argument total count
+*
+* @av: Pointer to arguments
+*
+* Return: Pointer to concatenated string (SUCCESS) or
+* NULL if @ac == 0 or @av == NULL (FAILURE) or
+* NULL if if insufficient memory was available (FAILURE)
+*/
 
-/**
- * strtow - splits a string into words.
- * @str: string.
- *
- * Return: pointer of an array of integers
- */
-char **strtow(char *str)
+char *argstostr(int ac, char **av)
 {
-	char **aout;
-	unsigned int c, height, i, j, a1;
+	int i, j;
+	int count = 0;
+	int t_count = 0;
+	char *result;
 
-	if (str == NULL || *str == '\0')
-		return (NULL);
-	for (c = height = 0; str[c] != '\0'; c++)
-		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-			height++;
-	aout = malloc((height + 1) * sizeof(char *));
-	if (aout == NULL || height == 0)
+	if (ac == 0 || av == NULL)
+		return ('\0');
+
+	for (i = 0; i < ac; i++)
 	{
-		free(aout);
-		return (NULL);
+		for (j = 0; av[i][j] != '\0'; j++)
+			t_count++;
+
+		t_count++;
 	}
-	for (i = a1 = 0; i < height; i++)
+
+	result = malloc(sizeof(char) * t_count + 1);
+
+	if (result == NULL)
 	{
-		for (c = a1; str[c] != '\0'; c++)
+		return ('\0');
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			if (str[c] == ' ')
-				a1++;
-			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-			{
-				aout[i] = malloc((c - a1 + 2) * sizeof(char));
-				if (aout[i] == NULL)
-				{
-					ch_free_grid(aout, i);
-					return (NULL);
-				}
-				break;
-			}
+			result[count++] = av[i][j];
 		}
-		for (j = 0; a1 <= c; a1++, j++)
-			aout[i][j] = str[a1];
-		aout[i][j] = '\0';
+		result[count++] = '\n';
 	}
-	aout[i] = NULL;
-	return (aout);
+
+	result[t_count] = '\0';
+	return (result);
 }
