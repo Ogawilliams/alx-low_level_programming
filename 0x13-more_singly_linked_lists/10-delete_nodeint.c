24fcfb5c58ae4a0388180934643c84caed9932ee
Williams Ichigboja <williamseichigboja@gmail.com>
2022-10-26 20:28:08 -0700

    Hot fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,42 +1,49 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes the node at index
- * @head: head of the linked list
- * @index: index to delete
- * Return: 1 if succeeded, -1 if failed
- */
+* insert_nodeint_at_index - inserts a new node at a given position
+* @head: double pointer to the head of the listint_t linked list
+* @idx: the index of the list where the new node should be added
+* @n: element/property n of the node to be added
+* Return: address of the new element (SUCCESS), or
+* NULL if it failed (FAILURE), or
+* NULL if is not possible to add the new node at index @idx
+*/
 
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *temp;
-	listint_t *rmovenode;
-	unsigned int i;
+	unsigned int i = 0;
+	listint_t *current_node = *head, *new_node_ptr;
 
-	i = 0;
-	temp = *head;
-	if (*head == NULL)
-		return (-1);
-	if (index == 0)
+	if (idx == 0)
 	{
-		*head = (*head)->next;
-		free(temp);
+		new_node_ptr = malloc(sizeof(listint_t));
+
+		if (!new_node_ptr)
+			return (NULL);
+		new_node_ptr->n = n;
+		new_node_ptr->next = *head;
+		*head = new_node_ptr;
+		return (new_node_ptr);
 	}
-	else
+
+	while (current_node && ((i + 1) != idx))
 	{
-		while (i < index - 1)
-		{
-			temp = temp->next;
-			if (temp == NULL)
-				return (-1);
-			i++;
-		}
-		rmovenode = temp;
-		rmovenode = rmovenode->next;
-		temp->next = rmovenode->next;
-		free(rmovenode);
+		current_node = current_node->next;
+		i++;
 	}
-	return (1);
+
+	if ((i + 1) == idx)
+	{
+		new_node_ptr = malloc(sizeof(listint_t));
+
+		if (!new_node_ptr)
+			return (NULL);
+		new_node_ptr->n = n;
+		new_node_ptr->next = current_node->next;
+		current_node->next = new_node_ptr;
+		return (new_node_ptr);
+	}
+	return (NULL);
 }
